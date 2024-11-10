0afca198cedf212207e2a343068faf0fad80fb23
adapole <eyassubn@gmail.com>
2024-11-10 22:12:14 +0300

    update: navbar

---
+++ frontend/app/connect.tsx
@@ -0,0 +1,185 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+// Particle imports
+import {
+	ConnectButton,
+	useAccount,
+	useDisconnect,
+	useParticleAuth,
+} from '@particle-network/connectkit';
+
+export default function Connect() {
+	// Initialize account-related states from Particle's useAccount hook
+	const { address, isConnected, isConnecting, isDisconnected, chainId } =
+		useAccount();
+	const { disconnect, disconnectAsync } = useDisconnect();
+	const { getUserInfo } = useParticleAuth();
+
+	// Define state variables
+	const [account, setAccount] = useState(null); // Store account information
+	const [balance, setBalance] = useState<string>(''); // Store user's balance
+	const [userAddress, setUserAddress] = useState<string>(''); // Store user's address
+	const [userInfo, setUserInfo] = useState<any>(null); // Store user's information
+	const [isLoadingUserInfo, setIsLoadingUserInfo] = useState<boolean>(false); // Loading state for fetching user info
+	const [userInfoError, setUserInfoError] = useState<string | null>(null); // Error state for fetching user info
+
+	// Connection status message based on the account's connection state
+	const connectionStatus = isConnecting
+		? 'Connecting...'
+		: isConnected
+		? 'Connected'
+		: isDisconnected
+		? 'Disconnected'
+		: 'Unknown';
+
+	// Load account details and fetch balance when address or chainId changes
+	useEffect(() => {
+		async function loadAccount() {
+			if (address) {
+				setAccount(account);
+				setUserAddress(address);
+				// await fetchBalance();
+			}
+		}
+		loadAccount();
+	}, [chainId, address]);
+
+	// Fetch and set user information when connected
+	useEffect(() => {
+		const fetchUserInfo = async () => {
+			setIsLoadingUserInfo(true);
+			setUserInfoError(null);
+
+			try {
+				const userInfo = await getUserInfo();
+				console.log(userInfo);
+				setUserInfo(userInfo);
+			} catch (error) {
+				setUserInfoError(
+					'Error fetching user info: The current wallet is not a particle wallet.'
+				);
+				console.error('Error fetching user info:', error);
+			} finally {
+				setIsLoadingUserInfo(false);
+			}
+		};
+
+		if (isConnected) {
+			fetchUserInfo();
+		}
+	}, [isConnected, getUserInfo]);
+
+	// Fetch user's balance and format it for display
+	//   const fetchBalance = async () => {
+	//     try {
+	//       if (!address) return;
+	//       const balanceResponse = await publicClient?.getBalance({ address });
+	//       const balanceInEther = formatEther(balanceResponse!);
+	//       console.log(balanceResponse);
+	//       setBalance(parseFloat(balanceInEther).toFixed(4)); // Display balance with 4 decimal places
+	//     } catch (error) {
+	//       console.error("Error fetching balance:", error);
+	//     }
+	//   };
+
+	// Handle user disconnect action
+	const handleDisconnect = async () => {
+		try {
+			await disconnectAsync();
+		} catch (error) {
+			console.error('Error disconnecting:', error);
+		}
+	};
+
+	// Parameters for the on-ramp URL
+	const fiatCoin = 'USD';
+	const cryptoCoin = 'ETH';
+	const network = 'Ethereum';
+	const theme = 'dark';
+	const language = 'en';
+
+	// Function to handle the on-ramp button click
+	const handleOnRamp = () => {
+		const onRampUrl = `https://ramp.particle.network/?fiatCoin=${fiatCoin}&cryptoCoin=${cryptoCoin}&network=${network}&theme=${theme}&language=${language}`;
+		window.open(onRampUrl, '_blank');
+	};
+
+	// Function to copy text to clipboard
+	const copyToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text).then(() => {
+			alert('Copied to clipboard!');
+		});
+	};
+
+	// Function to truncate Ethereum address
+	const truncateAddress = (address: string) => {
+		return address.slice(0, 6) + '...' + address.slice(-4);
+	};
+
+	return (
+		<div className=' flex flex-col items-center justify-between p-8 bg-black text-white'>
+			<main className='flex-grow flex flex-col items-center justify-center w-full max-w-6xl mx-auto'>
+				{isConnected ? (
+					<>
+						<div className='flex justify-center w-full'>
+							<div className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-4xl'>
+								<div className='border border-purple-500 p-6 rounded-lg'>
+									{isLoadingUserInfo ? (
+										<div>Loading user info...</div>
+									) : userInfoError ? (
+										<div className='text-red-500'>{userInfoError}</div>
+									) : (
+										userInfo && ( // Conditionally render user info
+											<div className='flex items-center'>
+												<h2 className='text-lg font-semibold text-white mr-2'>
+													Name: {userInfo.name || 'N/A'}
+												</h2>
+												{userInfo.avatar && (
+													<img
+														src={userInfo.avatar}
+														alt='User Avatar'
+														className='w-10 h-10 rounded-full'
+													/>
+												)}
+											</div>
+										)
+									)}
+									<h2 className='text-lg font-semibold mb-2 text-white flex items-center'>
+										Address: <code>{truncateAddress(userAddress)}</code>
+										<button
+											className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-1 px-2 ml-2 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg flex items-center'
+											onClick={() => copyToClipboard(userAddress)}
+										>
+											📋
+										</button>
+									</h2>
+									<h2 className='text-lg font-semibold mb-2 text-white'>
+										Chain ID: <code>{chainId}</code>
+									</h2>
+									<button
+										className='mt-4 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg'
+										onClick={handleOnRamp}
+									>
+										Buy Crypto with Fiat
+									</button>
+									<div>
+										<button
+											className='mt-4 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg'
+											onClick={handleDisconnect}
+										>
+											Disconnect
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</>
+				) : (
+					<ConnectButton />
+				)}
+			</main>
+		</div>
+	);
+}
--- frontend/app/navbar.tsx
+++ frontend/app/navbar.tsx
@@ -9,45 +9,121 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { Bell, CreditCard, User } from 'lucide-react';
+import { Bell, CopyIcon, User } from 'lucide-react';
+// Particle imports
+import {
+	ConnectButton,
+	useAccount,
+	useDisconnect,
+	useParticleAuth,
+} from '@particle-network/connectkit';
+import { useEffect, useState } from 'react';
 
 export default function NavBar() {
+	const { address, isConnected, isConnecting, isDisconnected, chainId } =
+		useAccount();
+	const { disconnect, disconnectAsync } = useDisconnect();
+	const { getUserInfo } = useParticleAuth();
+
+	const [userInfo, setUserInfo] = useState<any>(null);
+	const [account, setAccount] = useState(null);
+	const [userAddress, setUserAddress] = useState<string>('');
+
+	// Handle user disconnect action
+	const handleDisconnect = async () => {
+		try {
+			await disconnectAsync();
+		} catch (error) {
+			console.error('Error disconnecting:', error);
+		}
+	};
+
+	// Function to truncate Ethereum address
+	const truncateAddress = (address: string) => {
+		return address.slice(0, 6) + '...' + address.slice(-4);
+	};
+
+	const copyToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text).then(() => {
+			alert('Copied to clipboard!');
+		});
+	};
+
+	useEffect(() => {
+		async function loadAccount() {
+			if (address) {
+				setAccount(account);
+				setUserAddress(address);
+				// await fetchBalance();
+			}
+		}
+		loadAccount();
+	}, [chainId, address]);
+
+	useEffect(() => {
+		const fetchUserInfo = async () => {
+			try {
+				const userInfo = await getUserInfo();
+				console.log(userInfo);
+				setUserInfo(userInfo);
+			} catch (error) {
+				console.error('Error fetching user info:', error);
+			}
+		};
+
+		if (isConnected) {
+			fetchUserInfo();
+		}
+	}, [isConnected, getUserInfo]);
+
 	return (
 		<div className='flex items-center gap-2'>
 			<Button variant='ghost' size='icon'>
 				<Bell className='h-5 w-5' />
 				<span className='sr-only'>Notifications</span>
 			</Button>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-						<User className='h-6 w-6' />
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent className='w-56' align='end' forceMount>
-					<DropdownMenuLabel className='font-normal'>
-						<div className='flex flex-col space-y-1'>
-							<p className='text-sm font-medium leading-none'>John Doe</p>
-							<p className='text-xs leading-none text-muted-foreground'>
-								john@example.com
-							</p>
-						</div>
-					</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem>
-						<User className='mr-2 h-4 w-4' />
-						<span>Profile</span>
-					</DropdownMenuItem>
-					<DropdownMenuItem>
-						<CreditCard className='mr-2 h-4 w-4' />
-						<span>Billing</span>
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem>
-						<span>Log out</span>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
+			{isConnected ? (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+							<User className='h-6 w-6' />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent className='w-56' align='end' forceMount>
+						<DropdownMenuLabel className='font-normal'>
+							<div className='flex flex-col space-y-1'>
+								<p className='text-sm font-medium leading-none'>
+									{userInfo ? <span>{userInfo.name || 'N/A'}</span> : 'N/A'}
+								</p>
+								<p className='text-xs leading-none text-muted-foreground'>
+									Address: <code>{truncateAddress(userAddress)}</code>
+								</p>
+							</div>
+						</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem onClick={() => copyToClipboard(userAddress)}>
+							<CopyIcon className='mr-2 h-4 w-4' />
+							<span>
+								<code>{truncateAddress(userAddress)}</code>
+							</span>
+						</DropdownMenuItem>
+						{/* <DropdownMenuItem>
+							<CopyIcon
+								className='mr-2 h-4 w-4'
+								onClick={() => copyToClipboard(userAddress)}
+							/>
+						</DropdownMenuItem> */}
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<Button variant={null} onClick={handleDisconnect}>
+								<span>Disconnect</span>
+							</Button>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			) : (
+				<ConnectButton />
+			)}
 		</div>
 	);
 }
